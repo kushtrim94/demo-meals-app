844f5a359f19e8b87a43de416764677982b31ce1
Kushtrim Marke <kushtrimmarke12@gmail.com>
2023-08-11 17:23:25 +0200

    fixed loading text

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -42,7 +42,7 @@ const AvailableMeals = () => {
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
-        <p>Loading..</p>
+        <p>Loading...</p>
       </section>
     );
   }
